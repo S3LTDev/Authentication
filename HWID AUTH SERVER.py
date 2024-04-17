f11fb9733bd8a9513fb6fcfb40229a50c39f5584
S3LT <91411026+S3LTDev@users.noreply.github.com>
2024-04-17 12:52:41 +0100

    Update HWID AUTH SERVER.py

--- HWID AUTH SERVER.py
+++ HWID AUTH SERVER.py
@@ -1,11 +1,13 @@
-# oooo     oooo      o      ooooo  oooo   ooooooo   ooooooooooo ooooooooooo 
-# 8888o   888      888       888  88   o88     888 88  888  88 88    888   
-# 88 888o8 88     8  88        888           o888      888         888     
-# 88  888  88    8oooo88      88 888      o888   o     888       888    oo 
-# o88o  8  o88o o88o  o888o o88o  o888o o8888oooo88    o888o    o888oooo888                                                                         
-# link  https://github.com/max2tz/Authentication
-# author  max2tz https://github.com/max2tz
+#  @@@@@@ @@@@@@  @@@      @@@@@@@      @@@@@@@  @@@@@@@@ @@@  @@@
+# !@@         @@! @@!        @@!        @@!  @@@ @@!      @@!  @@@
+#  !@@!!   @!!!:  @!!        @!!        @!@  !@! @!!!:!   @!@  !@!
+#     !:!     !!: !!:        !!:        !!:  !!! !!:       !: .:! 
+# ::.: :  ::: ::  : ::.: :    :         :: :  :  : :: :::    ::   
+                                                                                                                              
+# link  https://github.com/S3LTDev/Authentication
+# author  max2tz https://github.com/S3LT
 # license  GPL-3.0 License 
+
 
 from flask import Flask, request
 from flask_mysql import MySQL
